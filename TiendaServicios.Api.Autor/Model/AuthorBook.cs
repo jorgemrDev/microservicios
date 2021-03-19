c22e9ab1504414bd302480cfc521b674101c8c98
Jorge Moreno <v-jomoreno@youngliving.com>
2021-03-19 13:04:17 -0500

    models , namespace

---
+++ TiendaServicios.Api.Autor/Model/AuthorBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace StoreServices.Api.Autor.Model
+{
+    public class AuthorBook
+    {
+        public int AuthorBookId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+        public ICollection<Grade> GradesList { get; set; }
+        public string AuthorBookGuid { get; set; }
+    }
+}
---
+++ TiendaServicios.Api.Autor/Model/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StoreServices.Api.Autor.Model
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string Name { get; set; }
+        public string AcademicCenter { get; set; }
+        public DateTime DateGrade { get; set; }
+
+        public int AuthorBookId { get; set; }
+        public AuthorBook AuthorBook { get; set; }
+        public string GradeGuid { get; set; }
+    }
+}
