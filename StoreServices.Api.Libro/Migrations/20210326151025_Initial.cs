7f3cae2eb1ecff9dfd41764e7072dead7924eaaa
Jorge Moreno <v-jomoreno@youngliving.com>
2021-03-26 10:15:40 -0500

    books microservice

---
+++ StoreServices.Api.Libro/Migrations/20210326151025_Initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreServices.Api.Libro.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Author = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ StoreServices.Api.Libro/Repository/LibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreServices.Api.Libro.Models;
+//dotnet ef migrations add Initial --project StoreServices.Api.Libro
+//dotnet ef database update --project StoreServices.Api.Libro
+namespace StoreServices.Api.Libro.Repository
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Book { get; set; }
+        
+    }
+}
